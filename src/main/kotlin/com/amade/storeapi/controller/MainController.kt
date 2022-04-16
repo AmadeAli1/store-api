0ad5e9ae3d102e860eaf0b5b1471a79bdcfd1f03
AmadeAli1 <aliamade29@gmail.com>
2022-04-16 13:52:33 +0200

    Second commit1

--- src/main/kotlin/com/amade/storeapi/controller/MainController.kt
+++ src/main/kotlin/com/amade/storeapi/controller/MainController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 import reactor.kotlin.core.publisher.toMono
 
 @RestController
-@RequestMapping("/main")
+@RequestMapping
 class MainController {
 
     @GetMapping("/hello")
