69751e40c5681ee8dabd68afebfd0949af5c966c
AmadeAli1 <aliamade29@gmail.com>
2022-04-18 09:23:55 +0200

    Exceptions

--- src/main/kotlin/com/amade/storeapi/model/Category.kt
+++ src/main/kotlin/com/amade/storeapi/model/Category.kt
@@ -1,6 +1,7 @@
 package com.amade.storeapi.model
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
@@ -8,6 +9,7 @@ import javax.validation.constraints.NotNull
 @Table("category")
 data class Category(
     @field:NotNull @field:NotBlank val name: String,
+    @field:NotNull @field:NotBlank @Column("image") var image: String,
 ) {
     @Id
     var id: Int = 0
