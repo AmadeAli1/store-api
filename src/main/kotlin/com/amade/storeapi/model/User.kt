775a7a170d8cbd27f134963a0eb8706ae4ed5955
AmadeAli1 <aliamade29@gmail.com>
2022-04-18 20:55:13 +0200

    Heroku deploy -> 20:54

--- src/main/kotlin/com/amade/storeapi/model/User.kt
+++ src/main/kotlin/com/amade/storeapi/model/User.kt
@@ -8,7 +8,7 @@ import javax.validation.constraints.NotNull
 
 @Table("usuario")
 data class User(
-    @field:NotBlank @field:NotNull @Id val id: String,
+    @field:NotBlank @field:NotNull @field:Id val id: String,
     @field:NotBlank @field:NotNull var name: String,
     @field:Email @field:NotBlank @field:NotNull var email: String,
     var image: String? = null,
